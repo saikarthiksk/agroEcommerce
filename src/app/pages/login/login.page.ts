b47590533a5af87485999b2e51a5ae13b7c3335c
me.saikarthik <me.saikarthik@gmail.com>
2021-10-19 00:13:40 +0530

    login page

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { User } from 'src/app/models/user';
+import { FirebaseAuthService } from 'src/app/services/firebase-auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  title = 'firebase-angular-auth';
+  isSignedIn = false
+  constructor(private firebaseService : FirebaseAuthService,
+    private auth:AngularFireAuth){
+    }
+  ngOnInit(){
+    if(localStorage.getItem('user')!== null){
+      let data=localStorage.getItem('userId');
+      console.log((data));
+      this.isSignedIn= true
+    }
+    else
+    this.isSignedIn = false
+  }
+  async onSignup(email:string,password:string){
+    await this.firebaseService.signup(email,password)
+    if(this.firebaseService.isLoggedIn)
+    this.isSignedIn = true
+  }
+  async onSignin(email:string,password:string){
+    await this.firebaseService.signin(email,password)
+    if(this.firebaseService.isLoggedIn)
+    this.isSignedIn = true
+  }
+  handleLogout(){
+    this.isSignedIn = false
+  }
+
+}
